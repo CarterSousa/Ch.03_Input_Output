d6d7351677b33dc3885c77162d8ee4057fb1fe09
CarterSousa <sousacar@urbandaleschools.com>
2022-10-03 12:15:03 -0500

    python looping

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,12 +1,16 @@
-# Sign your name:________________
+# Sign your name:Carter Sousa
 # In all the short programs below, do a good job communicating with your end user!
 
-
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
+import math
 
+username = input("hello! What is you name: ")
+print("hello "+username+" welcome to my calculation program!")
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
-
+base = input("What is the base of your triangle: ")
+height = input("what is the height of your triangle: ")
+int(1/2)(base*height)
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
 
